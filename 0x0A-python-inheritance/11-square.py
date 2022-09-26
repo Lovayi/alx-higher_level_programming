d8eb067686f48f2afc6c9a9ec0429ff73d713917
Lovayi <kevinphakathi@live.com>
2022-09-26 16:14:59 -0700

    Task 4

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""Square implementation based on Rectangle class
-    with __str__ method overriding"""
-
-
+"""Module contains implementation of `Square` class"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ private instance attr size is the size of the square
-        the __str__ method is overridden"""
-
+    """Inherits from class `Rectangle`"""
     def __init__(self, size):
+        """Initializes the values"""
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
+        super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        super().__str__()
-        return f"[Square] {self.__size}/{self.__size}"
+        """Prints the description of the `Rectangle`"""
+        string = "[Square] {}/{}".format(self.__size, self.__size)
+        return string
+
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""Implements a full rectangle based on BaseGeometry"""
-
+"""Contains a class that inherits from `BaseGeometry"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-        Full rectangle implementation
-        Args:
-        width - width of rectangle
-            height - height of rectangle
-    """
+    """Inherits from `BaseGeometry`"""
 
     def __init__(self, width, height):
+        """Initialize rectangle values"""
         self.integer_validator("width", width)
         self.__width = width
-
         self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        return self.__width * self.__height
+        """Calculates the area of the geometry"""
+        return (self.__width * self.__height)
 
     def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        """Prints the description of the `Rectangle`"""
+        string = "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return string
+
